f435cc8cf22e1dac689af02424c57886e31a3780
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-04-03 21:34:53 +0200

    Login

--- Company.web/Controllers/AccountController.cs
+++ Company.web/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Company.data.Models;
+﻿using Company.data.Models;
 using Company.web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +8,12 @@ namespace Company.web.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager , SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         public IActionResult SignUp()
         {
@@ -35,13 +36,43 @@ namespace Company.web.Controllers
                 var result = await _userManager.CreateAsync(user, input.Password);
 
                 if (result.Succeeded)
-                    return RedirectToAction("SignIn");
+                    return RedirectToAction("Login");
                 else
                 {
                     foreach (var error in result.Errors)
-                    {
                         ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(input);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(input.Email);
+
+                if (user is not null)
+                {
+                    if (await _userManager.CheckPasswordAsync(user , input.Password))
+                    {
+                        var result = await _signInManager.PasswordSignInAsync(user, input.Password, input.RememberMe, true);
+
+                        if (result.Succeeded)
+                            return RedirectToAction("Index", "Home");
+                        
                     }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(input);
                 }
             }
             return View(input);
