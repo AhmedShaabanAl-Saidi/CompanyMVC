66a13e8582fffb97490f94e0dbcd59a87e7d5044
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-04-03 23:24:30 +0200

    ResetPassword

---
+++ Company.web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.web.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])(?!.*(.)\1{1,})[A-Za-z\d@$!%*?&]{6,}$",
+        ErrorMessage = "Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number, one special character, and at least two unique characters.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+        public string Email { get; set; }
+    }
+}
